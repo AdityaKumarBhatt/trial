d6f84144f60e778fb2cd0fccb7c856850c9fda7e
AdityaKumarBhatt <59094192+AdityaKumarBhatt@users.noreply.github.com>
2021-10-12 13:43:36 +0530

    Add files via upload

---
+++ ecommerce-application/src/main/java/Servlets/LoginServlet.java
@@ -0,0 +1,47 @@
+package Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServelet
+ */
+@WebServlet("/LoginServelet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+	
+		System.out.println("email >>"+ email);
+		System.out.println("password >>"+ password);
+		
+		HttpSession s = request.getSession();
+		
+		s.setAttribute("current-user", s);
+		response.sendRedirect("UserRegistration.jsp");
+		
+		
+		
+	}
+
+}
